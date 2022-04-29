d4aaf9560a958dc8a66a61bad49b4c4626050e11
Sp00kyB00k <jelted@hotmail.n>
2022-04-29 15:10:16 +0200

    added a working basket with ajax for adding amount to it

--- basket/urls.py
+++ basket/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'basket'
 
 urlpatterns = [
-        path('', views.basket_summary, name='basket_summary')
+        path('', views.basket_summary, name='basket_summary'),
+        path('add/', views.basket_add, name="basket_add")
 ]
 
 
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+
+from .basket import Basket
+from store.models import Product
 
 def basket_summary(request):
     return render(request, 'store/basket/summary.html')
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+       product_id = int(request.POST.get('productid'))
+       product_qty = int(request.POST.get('productqty'))
+       product = get_object_or_404(Product, id=product_id) 
+       basket_qty = basket.__len__()
+       basket.add(product=product, qty=product_qty)
+       response = JsonResponse({'qty': basket_qty})
+       return response
+
+
