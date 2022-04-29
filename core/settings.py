8c76eb232a493f229bd01a5614974ab603d81453
Sp00kyB00k <jelted@hotmail.n>
2022-04-29 20:31:03 +0200

    production ready

--- core/settings.py
+++ core/settings.py
@@ -6,12 +6,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1^$a5!)*ntn*nx^e(l4t6bhdx2z*i!lm8yf3pf#xc%o7i6xn#z'
+try:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+except KeyError as e:
+    raise RuntimeError("Could not find a SECRET_KEY in environment") from e
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
