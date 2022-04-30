6bbebd8b675b63c57ede8e830ea2dbbbcd86e842
Jelte Derksen <jd@littleproductions.nl>
2022-04-30 09:19:26 +0000

    working in production

--- core/settings.py
+++ core/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/littleproductions/static'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
@@ -123,4 +124,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+"""
+# production configs
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+"""
